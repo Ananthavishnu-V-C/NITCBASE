16887f32f61c69ac7035a3ed563ab327a367392f
Ananthavishnu-V-C <ananthavishnu_b240139cs@nitc.ac.in>
2026-08-12 21:37:32 +0530

    Add files via upload

---
+++ exercieses/2_1.cpp
@@ -0,0 +1,66 @@
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+int main(int argc, char *argv[]) 
+{
+	Disk disk_run;
+
+	// create objects for the relation catalog and attribute catalog
+	RecBuffer relCatBuffer(RELCAT_BLOCK);
+	
+
+	HeadInfo relCatHeader;
+	HeadInfo attrCatHeader;
+
+	// load the headers of both the blocks into relCatHeader and attrCatHeader.
+	// (we will implement these functions later)
+	relCatBuffer.getHeader(&relCatHeader);
+	
+	
+
+	for (int i=0;i<relCatHeader.numEntries;i++) 
+	{
+
+		Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+		relCatBuffer.getRecord(relCatRecord, i);
+		printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+		
+		int temp=ATTRCAT_BLOCK;
+		
+		
+		//HeadInfo temp=attrCatHeader;
+		
+		while(temp!=-1)
+		{
+			
+			RecBuffer attrCatBuffer(temp);
+			attrCatBuffer.getHeader(&attrCatHeader);
+			
+
+			for (int j=0;j<attrCatHeader.numEntries;j++) 
+			{
+				
+				Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+				attrCatBuffer.getRecord(attrCatRecord,j);
+				// declare attrCatRecord and load the attribute catalog entry into it
+
+				if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal)==0) 
+				{
+					const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+					printf("  %s: %s\n",attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+				}
+				
+			}
+			
+			//printf("%d \n",attrCatHeader.rblock);
+			temp=attrCatHeader.rblock;
+			
+			
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
