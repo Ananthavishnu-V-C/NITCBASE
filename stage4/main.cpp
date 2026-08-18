a756ef95428daf3fe0cb38df4428f2dea879fa80
Ananthavishnu-V-C <ananthavishnu_b240139cs@nitc.ac.in>
2026-08-18 21:31:52 +0530

    Add main.cpp with disk and frontend interface setup

---
+++ stage4/main.cpp
@@ -0,0 +1,25 @@
+#include "Cache/OpenRelTable.h"
+#include "Disk_Class/Disk.h"
+#include "FrontendInterface/FrontendInterface.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) 
+{
+  Disk disk_run;
+  StaticBuffer buffer;
+  OpenRelTable cache;
+
+  return FrontendInterface::handleFrontend(argc, argv);
+}
+
+
+
+
+
+
+
+
+
+
+
+
